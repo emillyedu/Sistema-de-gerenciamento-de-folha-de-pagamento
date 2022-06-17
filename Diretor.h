709484c846696100209280ba02760e1e1e779972
Igor Matheus <72845501+Igor-Matheus@users.noreply.github.com>
2022-06-17 18:10:23 -0300

    Update Diretor.h

--- Diretor.h
+++ Diretor.h
@@ -2,13 +2,24 @@
 #define DIRETOR_H
 #include "GerenciaFuncionario.h"
 
-
+/*! Esta classe herda da classe abstrata "GerenciaFuncionario". É utilizada para calcular o aumento de salário
+ *  de um funcionário da designação Diretor.
+ *  \class
+*/
 class Diretor:public GerenciaFuncionario
 {
 public:
     Diretor();
     ~Diretor();
+
+    /*! Método sobrescrito que realiza o cálculo do aumento de salário para funcionários da designação Diretor.
+     *  Possui um parâmetro.
+     *  \param salario variável do tipo double, representando o salário do Diretor.
+     *  \return double, retorna o novo salário do Diretor após o aumento.
+     *  \author Lucas Bitu
+    */
     double calculaSalario(double salario);
+
 private:
 
 };
