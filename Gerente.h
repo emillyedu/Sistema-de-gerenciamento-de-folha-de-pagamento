edf28cebdd4c28f7d3f303256e59e0893d6201f3
Igor Matheus <72845501+Igor-Matheus@users.noreply.github.com>
2022-06-17 18:09:45 -0300

    Update Gerente.h

--- Gerente.h
+++ Gerente.h
@@ -2,12 +2,24 @@
 #define GERENTE_H
 #include "GerenciaFuncionario.h"
 
+/*! Esta classe herda da classe abstrata "GerenciaFuncionario". É utilizada para calcular o aumento de salário
+ *  de um funcionário da designação Gerente.
+ *  \class
+*/
 class Gerente:public GerenciaFuncionario
 {
 public:
     Gerente();
     ~Gerente();
+
+    /*! Um método sobrescrito que realiza o cálculo do aumento de salário para funcionários da designação Gerente.
+     *  Possui um parâmetro.
+     *  \param salario variável do tipo double, representando o salário do Gerente.
+     *  \return double, retorna o novo salário do Gerente após o aumento.
+     *  \author Lucas Bitu
+    */
     double calculaSalario(double salario);
+
 private:
 
 };
