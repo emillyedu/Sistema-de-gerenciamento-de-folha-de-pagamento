3f5ce26784237699b2ab91270cfdc7e24afd0ded
Igor Matheus <72845501+Igor-Matheus@users.noreply.github.com>
2022-06-17 18:08:59 -0300

    Update Operador.h

--- Operador.h
+++ Operador.h
@@ -2,13 +2,23 @@
 #define OPERADOR_H
 #include "GerenciaFuncionario.h"
 
+/*! Esta classe herda da classe abstrata "GerenciaFuncionario". É utilizada para calcular o aumento de salário
+ *  de um funcionário da designação Operador.
+ *  \class
+*/
 class Operador:public GerenciaFuncionario
 {
 public:
     Operador();
     ~Operador();
-    double calculaSalario(double salario);
 
+    /*! Tal método sobrescrito realiza o cálculo do aumento de salário para funcionários da designação Operador.
+     *  Possui um parâmetro.
+     *  \param salario variável do tipo double, representando o salário do Operador.
+     *  \return double, retorna o novo salário do Operador após o aumento.
+     *  \author Lucas Bitu
+    */
+    double calculaSalario(double salario);
 
 private:
 
