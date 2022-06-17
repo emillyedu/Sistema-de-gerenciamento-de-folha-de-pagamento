178ef1aa8ce61591832ef517f09147689a4d3ae0
Igor Matheus <72845501+Igor-Matheus@users.noreply.github.com>
2022-06-17 18:11:09 -0300

    Update Presidente.h

--- Presidente.h
+++ Presidente.h
@@ -2,12 +2,24 @@
 #define PRESIDENTE_H
 #include "GerenciaFuncionario.h"
 
+/*! Esta classe herda da classe abstrata "GerenciaFuncionario". É utilizada para calcular o aumento de salário
+ *  de um funcionário da designação Presidente.
+ *  \class
+*/
 class Presidente:public GerenciaFuncionario
 {
 public:
     Presidente();
     ~Presidente();
+
+    /*! Um método sobrescrito que realiza o cálculo do aumento de salário para funcionários da designação Presidente.
+     *  Possui um parâmetro.
+     *  \param salario variável do tipo double, representando o salário do Presidente.
+     *  \return double, retorna o novo salário do Presidente após o aumento.
+     *  \author Lucas Bitu
+    */
     double calculaSalario(double salario);
+
 private:
 
 };
