57e14f326cdf4cd94de1764434061dcd8c6a8c60
Igor Matheus <72845501+Igor-Matheus@users.noreply.github.com>
2022-06-17 21:08:58 -0300

    Update GerenciaFuncionario.h

--- GerenciaFuncionario.h
+++ GerenciaFuncionario.h
@@ -1,7 +1,8 @@
 #ifndef GERENCIAFUNCIONARIO_H
 #define GERENCIAFUNCIONARIO_H
 
-/*! Classe abstrata utilizada para ser herdada e auxiliar a gerenciar o salário dos funcionários de diferentes
+/** 
+ *  Classe abstrata utilizada para ser herdada e auxiliar a gerenciar o salário dos funcionários de diferentes
  *  designações.
  *  \class
 */
@@ -11,7 +12,8 @@ public:
     GerenciaFuncionario();
     ~GerenciaFuncionario();
 
-    /*! Tal método realiza o cálculo do aumento de salário, que é diferente, para as diferentes designações de
+    /** 
+     *  Tal método realiza o cálculo do aumento de salário, que é diferente, para as diferentes designações de
      *  funcionários. Isso, quando o método é sobrescrito nas classes filhas. Possui um parâmetro.
      *  \param salario variável do tipo double, representando o salário de um funcionário(a).
      *  \return double, retorna o novo salário do funcionário(a) após o aumento.
